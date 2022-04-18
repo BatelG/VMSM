d7169b7c415117125989bdbb5896d8922680e7b4
shamiroh <ohad.shamir@intel.com>
2022-04-18 21:57:27 +0300

    ohad commit

--- src/Gui.py
+++ src/Gui.py
@@ -1,13 +1,14 @@
 import os
 from tkinter import *
 from tkinter import filedialog
-
-from tkvideo import tkvideo
 import customtkinter
 from PIL import Image, ImageTk  # <- import PIL for the images
+from moviepy.editor import *
+from tkvideo import tkvideo
+
+from VMSM.src.resources.videoGenerator import VideoGenerator
 
 PATH = os.path.dirname(os.path.realpath(__file__))
-
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -22,8 +23,7 @@ class Video():
 
         path = self.video_loader_btn_handler()
         if path is not False:
-            player = tkvideo(path, my_label, loop=1, size=(450, 350))
-
+            player = tkvideo(path, my_label, loop=1, size=(350, 250))
             player.play()
 
     def video_loader_btn_handler(self):
@@ -31,26 +31,31 @@ class Video():
                                                    title="Select a File",
                                                    filetypes=(("Video files",
                                                                "*.mp4*"),
-                                                    ))
+                                                              ))
+
         if filename_path == "" or filename_path == " ":
             return False
 
         print(filename_path)
 
+        # clip = VideoFileClip(filename_path)
+        #
+        # # previewing the clip at fps = 10
+        # clip.without_audio().preview(fps=10)
+
         return str(filename_path)
 
 
 class App(customtkinter.CTk):
-
-    WIDTH = 650
-    HEIGHT = 750
+    WIDTH = 750
+    HEIGHT = 700
 
     def __init__(self):
         super().__init__()
 
         self.title("Video Motion Synchrony Measurement")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
-        #self.minsize(App.WIDTH, App.HEIGHT)
+        # self.minsize(App.WIDTH, App.HEIGHT)
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
 
@@ -72,24 +77,38 @@ class App(customtkinter.CTk):
         # load images as PhotoImage
         image_size = 30
 
-        file_explore_image = ImageTk.PhotoImage(Image.open(PATH + "\\resources\\images\\FileExplore.png").resize((image_size, image_size), Image.Resampling.LANCZOS))
+        file_explore_image = ImageTk.PhotoImage(
+            Image.open(PATH + "\\resources\\images\\FileExplore.png").resize((image_size, image_size),
+                                                                             Image.Resampling.LANCZOS))
+
+        report_image = ImageTk.PhotoImage(
+            Image.open(PATH + "\\resources\\images\\report_icon.png").resize((image_size, image_size),
+                                                                             Image.Resampling.LANCZOS))
 
         # ============ frame_left ============
 
         # configure grid layout (1x11)
-        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)  # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
 
         self.video_label = customtkinter.CTkLabel(master=self.frame_left,
-                                              text="Video Loader",
-                                              text_font=("Calibri Bold", -20))  # font name and size in px
+                                                  text="Video Loader",
+                                                  text_font=("Calibri Bold", -20))  # font name and size in px
         self.video_label.grid(row=1, column=0, pady=10, padx=10)
 
-        self.file_explore_btn = customtkinter.CTkButton(master=self.frame_left, image=file_explore_image, text="", width=30, height=30,
-                                   compound="right", command=self.video_handler)
+        self.file_explore_btn = customtkinter.CTkButton(master=self.frame_left, image=file_explore_image, text="",
+                                                        width=30, height=30,
+                                                        compound="right", command=self.video_handler)
         self.file_explore_btn.grid(row=2, column=0, pady=10, padx=20)
+
+        self.report_image_btn = customtkinter.CTkButton(master=self.frame_left, image=report_image, text="",
+                                                        width=30, height=30,
+                                                        compound="right", command=self.video_handler)
+        self.report_image_btn.grid(row=3, column=0, pady=10, padx=20)
+
+
 
         # self.button_2 = customtkinter.CTkButton(master=self.frame_left,
         #                                         text="CTkButton 2",
@@ -145,91 +164,82 @@ class App(customtkinter.CTk):
         self.radio_var = IntVar(value=0)
 
         self.roi_label = customtkinter.CTkLabel(master=self.frame_right,
-                                                        text="Select ROI:",
-                                                        text_font=("Calibri Bold", -20))
+                                                text="Select ROIs:",
+                                                text_font=("Calibri Bold", -20))
         self.roi_label.grid(row=0, column=2, columnspan=1, pady=20, padx=10, sticky="")
 
         self.right_hand_roi_choice = customtkinter.CTkRadioButton(master=self.frame_right,
-                                                            text="Right Hand",
-                                                           variable=self.radio_var,
-                                                           value=0)
+                                                                  text="Right Hand",
+                                                                  variable=self.radio_var,
+                                                                  value=0)
         self.right_hand_roi_choice.grid(row=1, column=2, pady=10, padx=20, sticky="n")
 
         self.left_hand_roi_choice = customtkinter.CTkRadioButton(master=self.frame_right,
-                                                            text="Left Hand  ",
-                                                           variable=self.radio_var,
-                                                           value=1)
+                                                                 text="Left Hand  ",
+                                                                 variable=self.radio_var,
+                                                                 value=1)
         self.left_hand_roi_choice.grid(row=2, column=2, pady=10, padx=20, sticky="n")
 
         self.pose_roi_choice = customtkinter.CTkRadioButton(master=self.frame_right,
                                                             text="Pose          ",
-                                                           variable=self.radio_var,
-                                                           value=2)
+                                                            variable=self.radio_var,
+                                                            value=2)
         self.pose_roi_choice.grid(row=3, column=2, pady=10, padx=20, sticky="n")
 
-        self.slider_1 = customtkinter.CTkSlider(master=self.frame_right,
-                                                from_=0,
-                                                to=1,
-                                                number_of_steps=3,
-                                                command=self.progressbar.set)
-        self.slider_1.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+        # self.slider_1 = customtkinter.CTkSlider(master=self.frame_right,
+        #                                         from_=0,
+        #                                         to=1,
+        #                                         number_of_steps=3,
+        #                                         command=self.progressbar.set)
+        # self.slider_1.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+
+        self.temp_label = customtkinter.CTkLabel(master=self.frame_right,
+                                                 text="Percentage Deviation: ",
+                                                 text_font=("Calibri Bold", -20))  # font name and size in px
+        self.temp_label.grid(row=4, column=0, columnspan=2, pady=10, sticky="NS")
+        # self.temp_label2 = customtkinter.CTkLabel(master=self.frame_right,
+        #                                           text="",
+        #                                           text_font=("Calibri Bold", -20))  # font name and size in px
+        # self.temp_label2.grid(row=4, column=2, columnspan=1, sticky="W")
 
         self.slider_2 = customtkinter.CTkSlider(master=self.frame_right,
-                                                command=self.progressbar.set)
-        self.slider_2.grid(row=5, column=0, columnspan=2, pady=10, padx=20, sticky="we")
-
-        self.slider_file_explore_btn = customtkinter.CTkButton(master=self.frame_right,
-                                                       height=25,
-                                                       text="CTkButton",
-                                                       command=self.button_event)
-        self.slider_file_explore_btn.grid(row=4, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+                                                command=self.MyHandler, from_=0, to=1)
+        self.slider_2.grid(row=5, column=0, columnspan=1, pady=10, padx=125, sticky="NS")
 
         self.slider_button_2 = customtkinter.CTkButton(master=self.frame_right,
-                                                       height=25,
-                                                       text="CTkButton",
+                                                       height=45,
+                                                       width=105,
+                                                       fg_color='gray',
+                                                       hover_color='green',
+                                                       text="Start Analysis",
+                                                       corner_radius=15,
+                                                       text_font=("Calibri Bold", -18),
                                                        command=self.button_event)
-        self.slider_button_2.grid(row=5, column=2, columnspan=1, pady=10, padx=20, sticky="we")
-
-        self.checkbox_file_explore_btn = customtkinter.CTkButton(master=self.frame_right,
-                                                         height=25,
-                                                         text="CTkButton",
-                                                         border_width=3,   # <- custom border_width
-                                                         fg_color=None,   # <- no fg_color
-                                                         command=self.button_event)
-        self.checkbox_file_explore_btn.grid(row=6, column=2, columnspan=1, pady=10, padx=20, sticky="we")
-
-        self.check_box_1 = customtkinter.CTkCheckBox(master=self.frame_right,
-                                                     text="CTkCheckBox")
-        self.check_box_1.grid(row=6, column=0, pady=10, padx=20, sticky="w")
-
-        self.check_box_2 = customtkinter.CTkCheckBox(master=self.frame_right,
-                                                     text="CTkCheckBox")
-        self.check_box_2.grid(row=6, column=1, pady=10, padx=20, sticky="w")
+        self.slider_button_2.grid(row=6, column=0, columnspan=3, pady=10, padx=135, sticky="W")
 
         # self.entry = customtkinter.CTkEntry(master=self.frame_right,
         #                                     width=120,
         #                                     placeholder_text="CTkEntry")
         # self.entry.grid(row=8, column=0, columnspan=2, pady=20, padx=20, sticky="we")
 
-        self.button_5 = customtkinter.CTkButton(master=self.frame_right,
-                                                text="CTkButton",
-                                                command=self.button_event)
-        self.button_5.grid(row=8, column=2, columnspan=1, pady=20, padx=20, sticky="we")
-
         # set default values
         self.right_hand_roi_choice.select()
         self.switch_2.select()
-        self.slider_1.set(0.2)
-        self.slider_2.set(0.7)
-        self.progressbar.set(0.5)
-        self.slider_file_explore_btn.configure(state=DISABLED, text="Disabled Button")
-        #self.pose_roi_choice.configure(state=tkinter.DISABLED)
-        self.check_box_1.configure(state=DISABLED, text="CheckBox disabled")
-        self.check_box_2.select()
+        # self.slider_1.set(0.2)
+        self.slider_2.set(0.0)
+        self.progressbar.set(0.0)
+        # self.pose_roi_choice.configure(state=tkinter.DISABLED)
+
+    def MyHandler(self, val):
+        #  self.progressbar.set(val)
+        self.temp_label.set_text('Percentage Deviation: %.2f' % val)
 
     def video_handler(self):
         video_frame = customtkinter.CTkFrame(self.frame_info)
+        #path = self.video_loader_btn_handler()
         video_frame.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+        #VideoGenerator(video_frame)
+
         Video(video_frame)
 
     def button_event(self):
---
+++ src/resources/videoGenerator.py
@@ -0,0 +1,97 @@
+import os
+import time
+import tkinter as tk
+from tkinter import filedialog
+
+import imageio
+from PIL import Image, ImageTk
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+global pause_video
+
+# download video at: http://www.html5videoplayer.net/videos/toystory.mp4
+
+
+video_path = "toystory.mp4"
+
+
+class VideoGenerator:
+
+    def __init__(self, frame):
+        # root = tk.Tk()
+        path = self.video_loader_btn_handler()
+        if path is not False:
+            self.video = imageio.get_reader(self.video_loader_btn_handler())
+            root = frame
+            root.title('Video in tkinter')
+            my_label = tk.Label(root)
+            my_label.pack()
+            tk.Button(root, text='start', command=self._start).pack(side=tk.LEFT)
+            tk.Button(root, text='stop', command=self._stop).pack(side=tk.LEFT)
+
+            pause_video = False
+            movie_frame = self.video_frame_generator()
+
+            while True:
+                if not pause_video:
+                    frame_number, frame = next(movie_frame)
+                    my_label.config(image=frame)
+
+                root.update()
+
+
+    def video_frame_generator(self):
+        def current_time():
+            return time.time()
+
+        start_time = current_time()
+        _time = 0
+        for frame, image in enumerate(self.video.iter_data()):
+
+            # turn video array into an image and reduce the size
+            image = Image.fromarray(image)
+            image.thumbnail((750, 750), Image.ANTIALIAS)
+
+            # make image in a tk Image and put in the label
+            image = ImageTk.PhotoImage(image)
+
+            # introduce a wait loop so movie is real time -- asuming frame rate is 24 fps
+            # if there is no wait check if time needs to be reset in the event the video was paused
+            _time += 1 / 24
+            run_time = current_time() - start_time
+            while run_time < _time:
+                run_time = current_time() - start_time
+            else:
+                if run_time - _time > 0.1:
+                    start_time = current_time()
+                    _time = 0
+
+            yield frame, image
+
+    def _stop(self):
+        global pause_video
+        pause_video = True
+
+    def _start(self):
+        global pause_video
+        pause_video = False
+
+    def video_loader_btn_handler(self):
+        PATH = os.path.dirname(os.path.realpath(__file__))
+        filename_path = filedialog.askopenfilename(initialdir=PATH,
+                                                   title="Select a File",
+                                                   filetypes=(("Video files",
+                                                               "*.mp4*"),
+                                                              ))
+
+        if filename_path == "" or filename_path == " ":
+            return False
+
+        print(filename_path)
+
+        clip = VideoFileClip(filename_path)
+
+        # previewing the clip at fps = 10
+        clip.without_audio().preview(fps=10)
+        return False
+        return str(filename_path)
