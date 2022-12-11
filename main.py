2554f3d8391f65512eee79ed5db8114fd8c54d66
BatelG <batelg06@gmail.com>
2022-12-11 23:14:11 +0200

    create raw data txt files

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import pandas as pd
 import numpy as np
 
 def main():
-    # app = App()
-    # app.start()
+    app = App()
+    app.start()
 
     ###############
-    data = {
-        "calories": [420, 380, 390],
-        "duration": [50, 40, 45]
-    }
+    # data = {
+    #     "calories": [420, 380, 390],
+    #     "duration": [50, 40, 45]
+    # }
 
-    df = pd.DataFrame(data)
+    # df = pd.DataFrame(data)
 
-    np.savetxt('raw_data_txt.txt', df.values, fmt='%d')
-    df.to_csv('raw_data_csv.csv')
+    # np.savetxt('raw_data_txt.txt', df.values, fmt='%d')
+    # df.to_csv('raw_data_csv.csv')
 
 
 def double_cropping():
